c29ce9d3164e028d8991e800729b0babe3be8b75
roberto <robertocarta.mail@gmail.com>
2018-10-26 21:37:51 +0200

    webdev shit

--- django.vim
+++ django.vim
@@ -1,3 +1,4 @@
 
 nnoremap  <C-d> :split<CR>:terminal python mgmt/manage.py test
+nnoremap  <C-d>t :split<CR>:terminal python mgmt/manage.py test 
 autocmd BufNewFile,BufRead *.jinja2 set syntax=htmldjango
--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 cp ~/dotfiles/vimrc ~/.vimrc
+
+cp ~/dotfiles/inputrc ~/.inputrc
+cp ~/dotfiles/bash_profile ~/.bash_profile
+cp ~/dotfiles/pylintrc ~/.pylintrc
+
 cp ~/dotfiles/init.vim ~/.config/nvim/init.vim
+
+pip3 install -r requirements.txt
+pip install -r requirements.txt
+
+
 nvim -c "PlugClean"
 nvim -c "PlugInstall"
 echo "Done."
